8a6310cd9d69fa065bf3f370274af85bddb71576
= <creazyhell@gmail.com>
2025-03-07 14:13:06 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: fileURLToPath(new URL('./src/main.ts', import.meta.url)),
+      name: 'VFloat',
+      fileName: (format) => `v-float.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
