284f4e572eefcb1418e868c8dbb316a3d6c61c88
shareef <sherif.hassan.dev@email.com>
2025-04-01 09:39:50 +0200

    feat(types): add Fn type for a no-argument function

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,2 @@
-export type AnyFn<T extends unknown[] = unknown[], U = unknown> = (...args: T) => U;
+export type AnyFn<T extends unknown[] = unknown[], U = unknown> = (...args: T) => U
+export type Fn = () => void
