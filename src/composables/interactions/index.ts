cec7b6288f37a01575f3bda67c50dffdd4336714
Shareef Hassan <creazyhell@gmail.com>
2025-09-27 07:20:24 +0300

    refactor(use-client-point): export types and function explicitly

--- src/composables/interactions/index.ts
+++ src/composables/interactions/index.ts
@@ -1,5 +1,13 @@
 export * from "./use-click"
-export * from "./use-client-point"
+export {
+  type AxisConstraint,
+  type Coordinates,
+  type TrackingMode,
+  type UseClientPointContext,
+  type UseClientPointOptions,
+  type UseClientPointReturn,
+  useClientPoint,
+} from "./use-client-point"
 export * from "./use-escape-key"
 export * from "./use-focus"
 export * from "./use-hover"
