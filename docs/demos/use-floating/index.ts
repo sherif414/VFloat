b99f216a42ece17f9994d5520966b27953201e1d
sherif414 <creazyhell@gmail.com>
2026-03-31 12:35:47 +0200

    chore(docs): remove unused demo files

--- docs/demos/use-floating/index.ts
+++
@@ -1,9 +0,0 @@
-import "../../../env.js";
-export { default as ClientPointDemo } from "./ClientPointDemo.vue";
-export { default as DropdownMenu } from "./DropdownMenu.vue";
-export { default as Interaction } from "./Interaction.vue";
-export { default as InteractiveTooltip } from "./InteractiveTooltip.vue";
-export { default as ModalDialog } from "./ModalDialog.vue";
-export { default as PlacementDemo } from "./PlacementDemo.vue";
-export { default as UseFloatingBasicUsage } from "./UseFloatingBasicUsage.vue";
-export { default as WithMiddleware } from "./WithMiddleware.vue";
